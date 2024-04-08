87b36e52def872b9808b6b75df599c8691b49b55
Wouter de Bruijn <wouter@netco.nl>
2024-04-08 13:03:45 +0200

    ✨ Added neightbor and interface list to detail view

--- netgazer/viewsets/device_viewset.py
+++ netgazer/viewsets/device_viewset.py
@@ -1,10 +1,26 @@
 from rest_framework.viewsets import ModelViewSet
-from netgazer.models import Device
-from netgazer.serializers import DeviceSerializer
+from netgazer.models import Device, Interface, Neighbor
+from netgazer.serializers import DeviceSerializer, InterfaceSerializer, NeighborSerializer
+from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.response import Response
 from rest_framework import permissions
 
 
-class DeviceViewSet(ModelViewSet):
+class RetrieveDeviceMixin(RetrieveModelMixin):
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        interfaces = Interface.objects.filter(device=instance)
+        neighbors = Neighbor.objects.filter(device=instance)
+
+        interfaces = InterfaceSerializer(interfaces, many=True).data
+        neighbors = NeighborSerializer(neighbors, many=True).data
+
+        return Response(serializer.data | {'interfaces': interfaces, 'neighbors': neighbors})
+
+
+class DeviceViewSet(ModelViewSet, RetrieveDeviceMixin):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
     permission_classes = [permissions.IsAdminUser]
