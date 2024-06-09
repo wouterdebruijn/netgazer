be8194219d30f2e3e33b6d3a80d1512ce2863a4f
Wouter de Bruijn <wouter@netco.nl>
2024-06-09 11:50:22 +0200

    🗃️  Changes to datamodel

--- netgazer/models/neighbor.py
+++ netgazer/models/neighbor.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Neighbor(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     ipv4 = models.GenericIPAddressField(null=True)
     ipv6 = models.GenericIPAddressField(null=True)
 
@@ -12,6 +12,8 @@ class Neighbor(models.Model):
     device = models.ForeignKey('Device', on_delete=models.CASCADE)
     neighbor_device = models.ForeignKey(
         'Device', on_delete=models.CASCADE, related_name='neighbor_device', null=True)
+
+    port_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
